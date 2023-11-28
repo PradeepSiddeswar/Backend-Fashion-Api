f687ad94ed7cbe785877615fc64185a2d5cd84eb
FormApi <siddeswarapradeep@gmail.com>
2023-11-28 11:18:33 +0530

    update

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require("express")
+const dotenv = require("dotenv")
+const cors = require("cors")
+const path = require("path")
+const app = express()
+
+app.use(cors({
+  origin: 'http://localhost:3000/',
+  methods:['GET', 'POT', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+
+}))
+
+
+const CategoryDetailsRoute = require('./Routes/CategoryDetails_Router')
+
+const bp = require("body-parser")
+app.use(bp.json());
+app.use(bp.urlencoded({extended:false}));
+
+
+dotenv.config({ path: '.env'})
+const PORT = process.env.PORT || 8080
+console.log("Server Started", PORT)
+const mongoose = require("mongoose");
+mongoose.pluralize(null)
+
+mongoose.connect(process.env.MONGO_URL, {
+
+})
+.then(() => console.log(`Connected To Database`))
+.then(() => {
+    app.listen(PORT);
+})
+.catch((error) => console.log(error));
+
+
+app.get("/", (req, res) => {
+    res.send("Hello world")
+})
+
+app.use(express.static("upload"))
+app.use('/Category-Details', CategoryDetailsRoute)
