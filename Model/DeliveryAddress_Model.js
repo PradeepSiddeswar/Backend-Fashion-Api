7bf0d33d5cf8003d420ec6607cff094e1c398a25
FormApi <siddeswarapradeep@gmail.com>
2023-12-08 15:37:08 +0530

    update

--- Model/DeliveryAddress_Model.js
+++ Model/DeliveryAddress_Model.js
@@ -9,20 +9,20 @@ const deliveryaddressSchema = new mongoose.Schema({
         type: Number,
         required: true
     },
-    Pincode: {
-        type: Number,
-        required: true
-    },
-    AddressHouseNo: {
+    // Pincode: {
+    //     type: Number,
+    //     required: true
+    // },
+    Address: {
         type: String,
     },
-    City: {
-        type: String,
-    },
-    State: {
-        type: String,
-        required: true
-    },
+    // City: {
+    //     type: String,
+    // },
+    // State: {
+    //     type: String,
+    //     required: true
+    // },
     Name: {
         type: String,
         required: true
